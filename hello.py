d99517b7a6a6c3a72fcb31a1ae7f631a2cb95892
Michael Murray <michael@freeassoc.net>
2013-04-14 16:23:08 -0700

    corr. to Bottle framework: using views

--- hello.py
+++ hello.py
@@ -5,20 +5,10 @@ import pymongo
 # this is the handler for the default path of the web server
 
 @bottle.route('/')
-def index():
-    # connect to mongoDB
-    connection = pymongo.MongoClient('localhost',27017)
-
-    # attach to test database
-    db = connection.test
-
-    # get handle for names collection
-    name = db.names
-
-    # find a single document
-    item = name.find_one()
-
-    return '<b>Hello to %s</b>' % item['name']
+def home_page():
+  mythings = [ 'apple', 'orange', 'banana', 'peach' ]
+  # return bottle.template('hello_world', username='Michael', things=mythings)
+  return bottle.template('hello_world', { 'username' :'Michael', 'things' : mythings} )
 
 @bottle.route('/testpage')
 def testpage():
