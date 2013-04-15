3872796a3be23829abebfc3715aaab5943b14ec2
Michael Murray <michael@freeassoc.net>
2013-04-14 17:06:42 -0700

    Chapter 1 Pymongo exception processing

---
+++ mongo_exception.py
@@ -0,0 +1,31 @@
+import sys
+import pymongo
+
+def main():
+  
+  connection = pymongo.MongoClient("mongodb://localhost")
+
+  db = connection.test
+  users = db.users
+
+  doc = {'firstname':'Andrew', 'lastname':'Erlichson'}
+  print doc
+  print "about to insert the document"
+
+  try:
+    users.insert(doc)
+  except:
+    print "insert failed", sys.exc_info()[0]
+
+  print doc
+  print "inserting again"
+
+  try:
+    users.insert(doc)
+  except:
+    print "insert failed", sys.exc_info()[0]
+
+main()
+
+
+
